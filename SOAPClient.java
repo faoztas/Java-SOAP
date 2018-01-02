b753556bacd1b24f56772d7b0c9a8f8e19161ea5
Furkan Anıl ÖZTAŞ <trpc61@gmail.com>
2018-01-03 02:24:18 +0300

    finished homework

--- SOAPClient.java
+++ SOAPClient.java
@@ -1,36 +1,85 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.soap.*;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
 public class SOAPClient {
+
     String SOAP_ENDPOINT;
-	String SOAP_ACTION;
+    String SOAP_ACTION;
     String TARGET_NAMESPACE;
     String OPERATION_NAME;
     String PARAMETER_NAME;
     String PARAMETER_VALUE;
 
     public static void main(String args[]) {
-        String endpoint = "http://81.214.73.178/TahsilatService/TahsilatService.asmx";
-        String namespace = "http://tempuri.org/";
-        String operation = "BorcSorgu";
-        String parameter = "referansNo";
+        final String endpoint = "http://81.214.73.178/TahsilatService/TahsilatService.asmx";
+        final String namespace = "http://tempuri.org/";
+        final String operation = "BorcSorgu";
+        final String parameter = "referansNo";
         //String value = "58302861634";
-        String value = "30295872165";
-        
-        SOAPClient client = new SOAPClient(endpoint, namespace, operation, parameter, value);
-        client.callSoapWebService();
-    }
-    
-    public SOAPClient(String endpoint, String namespace, String operation, String parameter, String value) {
-    	SOAP_ENDPOINT = endpoint;
-    	SOAP_ACTION = namespace + operation;
-    	TARGET_NAMESPACE = namespace;
-    	OPERATION_NAME = operation;
-    	PARAMETER_NAME = parameter;
-    	PARAMETER_VALUE = value;
+        //String value = "30295872165";
+
+        JFrame frame = new JFrame();
+        //JFrame settings
+
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        //Table
+        final DefaultTableModel model = new DefaultTableModel();
+        Object[] columNames = {"BorcReferansNo","GelirID","BorcTur","DonemTaksit","SonOdemeTarih","Tutar","Gecikme","Toplam"};
+        model.setColumnIdentifiers(columNames);
+        JTable table = new JTable();
+        table.setSize(1000,1000);
+        table.setModel(model);
+
+        //Textfield
+        final JTextField textField = new JTextField(8);
+
+        //Button
+        JButton button = new JButton("Sorgula");
+        button.setSize(60,30);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SOAPClient client = new SOAPClient(endpoint, namespace, operation, parameter, textField.getText());
+                client.callSoapWebService(model);
+            }
+        });
+
+        //Last Settings
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setSize(750,600);
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.add(button);
+        frame.add(textField);
+
+        frame.setVisible(true);
+
     }
 
-    public void callSoapWebService() {
+    public SOAPClient(String endpoint, String namespace, String operation, String parameter, String value) {
+        SOAP_ENDPOINT = endpoint;
+        SOAP_ACTION = namespace + operation;
+        TARGET_NAMESPACE = namespace;
+        OPERATION_NAME = operation;
+        PARAMETER_NAME = parameter;
+        PARAMETER_VALUE = value;
+    }
+
+    public void callSoapWebService(DefaultTableModel model) {
         try {
             // Create SOAP Connection
             SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
@@ -43,10 +92,27 @@ public class SOAPClient {
             System.out.println("Response SOAP Message:");
             //soapResponse.writeTo(System.out);
             //System.out.println();
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            soapResponse.writeTo(stream);
-        	System.out.println(new String(stream.toByteArray()));
-            
+            ByteArrayOutputStream outputstream = new ByteArrayOutputStream();
+            soapResponse.writeTo(outputstream);
+            System.out.println(new String(outputstream.toByteArray()));
+
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+            ByteArrayInputStream instream = new ByteArrayInputStream(outputstream.toByteArray()); //outputstream to inputstream
+
+            Document document = documentBuilder.parse(instream);
+
+            NodeList borcdetaylist =  document.getElementsByTagName("BorcDetay");
+
+            Object[] rows = new Object[8];
+            for(int i=1;i<borcdetaylist.getLength();i++){
+                for(int j=0;j<borcdetaylist.item(i).getChildNodes().getLength();j++){
+                   rows[j] = borcdetaylist.item(i).getChildNodes().item(j).getTextContent();
+                }
+                model.addRow(rows);
+            }
+
 
             soapConnection.close();
         } catch (Exception e) {
